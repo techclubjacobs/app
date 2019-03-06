8287adad7a61f0532a8f9b04ddabe27ced8ee7fd
Malte Granderath <22001111+mgranderath@users.noreply.github.com>
2019-03-06 17:32:08 +0100

    Setting up the different projects and Boilerplate (#9)
    * setup the basic project structure
    
    * changed to general ci script

---
+++ life/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { AppHeader, AppLink, AppLogo, AppWrapper } from "./styles/get-started-components";
+import { GlobalStyled } from "./styles/global";
+
+class App extends Component<{}, {}> {
+  public render() {
+    return (
+      <AppWrapper>
+        <GlobalStyled/>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <AppLink
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </AppLink>
+        </AppHeader>
+      </AppWrapper>
+    );
+  }
+}
+
+export default App;
