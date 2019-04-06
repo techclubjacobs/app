0872b0d50f0a4aacfd507dbc8fe08d76eb8666fc
ArsenijPercov <a.percov@jacobs-university.de>
2019-04-06 20:49:35 +0200

    Create prototype of CreateView form

--- life/README.md
+++ life/README.md
@@ -1,4 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Install packages
+
+In the project directory, you can run:
+### `npm install`
 
 ## Available Scripts
 
--- store/src/components/CreateView.tsx
+++ store/src/components/CreateView.tsx
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 
+
 class CreateView extends Component<{}, {}> {
+    constructor(props:any){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event:any){
+        event.preventDefault();
+        var title = this.refs.title
+        var category = this.refs.category
+        var price = this.refs.price
+        var quantity = this.refs.quantity 
+        var result = "Ttile: " + title + " Price: " + price + "Quantity: " + quantity
+        alert(result);
+        
+    }
     public render() {
         return (
-            <div>CreateView</div>
+            <div>
+                <h2> Sell Item</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" ref="title" placeholder="Title"/>
+                    <select id="evcat" ref="category">
+                        <option value="Test">test</option>
+                    </select>
+                    <input type="number" ref="price" placeholder="20"/>
+                    <input type="number" ref="quantity" placeholder="2"/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         );
     }
 }
