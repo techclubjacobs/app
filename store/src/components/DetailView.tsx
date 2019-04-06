f073b14fa21e24483b3a39c43fc3138506b95463
zohairajmal <zohairajmal1@gmail.com>
2019-04-06 22:10:29 +0200

    changes sizing of modal to dynamic for mobile devices

--- store/src/components/DetailView.tsx
+++ store/src/components/DetailView.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import Modal from 'react-responsive-modal';
 import jsonData from "./details.json";
+import './style.css'
 console.log(jsonData);
 
 class DetailView extends Component<{}, {}> {
@@ -24,7 +25,9 @@ class DetailView extends Component<{}, {}> {
       return (
         <div>
           <button onClick={this.onOpenModal}>Click to view Details</button>
+          <div className = "modalClass">
           <Modal open={open} onClose={this.onCloseModal} center>
+          <div className = "childDiv">
           <h1> DETAILS OF ITEM </h1>
             <img src={obj.image} alt=""></img>
             <h2>Product ID:{obj.product_id}</h2>
@@ -36,7 +39,9 @@ class DetailView extends Component<{}, {}> {
             <h2>Active:{obj.active}</h2>
             <h2>Sold:{obj.sold}</h2>
             <h2>Quantity:{obj.quantity}</h2>
+              </div>
           </Modal>
+          </div>
         </div>
       );
     }
