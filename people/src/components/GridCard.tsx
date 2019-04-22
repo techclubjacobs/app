ffc78424cd139a1650cf6c8f3546b8cb19eb3ca2
Lorenzo Rota <lorenzo.rota@hotmail.com>
2019-04-22 12:01:43 +0200

    Initial gridcard (unstyled, not in container)

--- people/src/components/GridCard.tsx
+++ people/src/components/GridCard.tsx
@@ -1,9 +1,26 @@
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@material-ui/core";
 import React, { Component } from "react";
+
+const listStyle = {
+  margin: "0 auto",
+  width: "50%",
+};
+
+const avatarStyle = {
+  height: "100px",
+  width: "100px",
+};
 
 class GridCard extends Component<{}, {}> {
   public render() {
     return (
-        <span>gridcard</span>
+      <List style={listStyle}>
+        <ListItem alignItems="center">
+          <ListItemAvatar>
+            <Avatar alt="Avatar" src="default_profile.gif" style={avatarStyle} />
+          </ListItemAvatar>
+        </ListItem>
+      </List>
     );
   }
 }
