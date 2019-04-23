c22be83e1171ed0eb5c701e926deca30762c02c3
Lorenzo Rota <lorenzo.rota@hotmail.com>
2019-04-23 20:12:17 +0200

    Basic gridCard container with a fixed amount of dummy gridCards

--- people/src/components/GridCard.tsx
+++ people/src/components/GridCard.tsx
@@ -1,7 +1,9 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@material-ui/core";
+import { Avatar, List, ListItem, ListItemAvatar, Grid, Paper } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
 import React, { Component } from "react";
 
-const listStyle = {
+const gridStyle = {
   margin: "0 auto",
   width: "50%",
 };
@@ -11,18 +13,47 @@ const avatarStyle = {
   width: "100px",
 };
 
+const styles = (theme: any) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 100,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
+
 class GridCard extends Component<{}, {}> {
+  public state = {
+    spacing: '12',
+    gridCount: '2'
+  };
+
   public render() {
+    const { classes }: any = this.props;
+    const { spacing } = this.state;
+
     return (
-      <List style={listStyle}>
-        <ListItem alignItems="center">
-          <ListItemAvatar>
-            <Avatar alt="Avatar" src="default_profile.gif" style={avatarStyle} />
-          </ListItemAvatar>
-        </ListItem>
-      </List>
+      <Grid container style={gridStyle} className={classes.root} spacing={16}>
+        <Grid item xs={12}>
+          <Grid container className={classes.test} justify="center" spacing={16}>
+          {[1,2,3,4,5,6,7,8,9,10].map(value => (
+            <Grid key={value} item>
+              <Paper className={classes.paper}>
+              <ListItemAvatar>
+                  <Avatar alt="Avatar" src="default_profile.gif" style={avatarStyle} />
+              </ListItemAvatar>
+              </Paper>
+            </Grid>
+          ))}
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default GridCard;
+export default withStyles(styles)(GridCard);
