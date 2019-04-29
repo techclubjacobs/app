d61b3ba475d6908ca361b0ca40ca8bad84a87a5a
zohairajmal <zohairajmal1@gmail.com>
2019-04-29 13:51:42 +0200

    Displaying contents of json file for chat and differentiating between me and you messages

--- store/src/pages/Chat.tsx
+++ store/src/pages/Chat.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import jsonData from "./chat.json";
 
 class ChatPage extends Component<{}, {}> {
+    
     public render() {
+
         return (
             <div>
-                <b>Here is the chat page</b>
+                <h1>Sender ID : {jsonData.sender_id}</h1>
+                <h1>Receiver ID : {jsonData.receiver_id}</h1>
+                {jsonData.messages.map(me => me.is_me ? <h3> Me :{me.text} <h4> Time: {me.timestamp} </h4> </h3> : <h3>Other Person: {me.text}<h4> Time: {me.timestamp} </h4></h3>)}
             </div>
         );
     }
