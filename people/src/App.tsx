56f3508ed1ab25b02b7e27a0d08b20fdf5748339
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2019-07-06 00:48:08 +0200

    :art: use ListCardView #29

--- people/src/App.tsx
+++ people/src/App.tsx
@@ -1,23 +1,18 @@
 import React, { Component } from "react";
 import GridCard from "./components/GridCard";
-import ListCard from "./components/ListCard";
 import SearchBar from "./components/SearchBar";
 import { AppWrapper } from "./styles/get-started-components";
+import ListCardView from "./components/ListCardView/ListCardView";
 
 class App extends Component<{}, {}> {
   public render() {
     return (
-      <AppWrapper>
+      <React.Fragment>
         <div style={{ width: "50%", margin: "0 auto" }}>
           <SearchBar></SearchBar>
         </div>
-
-        <div>
-          <GridCard />
-          <br />
-          <ListCard />
-        </div>
-      </AppWrapper>
+        <ListCardView />
+      </React.Fragment>
     );
   }
 }
